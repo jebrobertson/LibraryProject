86da2c5d9a183ba7623fc100713f5f00ec919de9
root <root@ip-172-31-87-177.ec2.internal>
2019-03-21 18:40:38 +0000

    Got php to work. To make it work on all machines download yum install php then add the conf file to /etc/httpd/conf.d/welcome.conf

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+<head>
+	<title>
+		Hello Gary
+	</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<p>Hello World </p>
+	<?php
+	echo "hello";
+	phpinfo();
+	?>
+</body>
+</html>
